cf23ce92244fbf6ec5f493d2636930f4f77b5928
Mitchel Falcon <17913494+mitchelfalcon@users.noreply.github.com>
2025-12-25 21:33:40 -0700

    Kaggle Notebook | notebookd5b3f3eb76 | Version 3

--- 04_tokenizer_kanish.py
+++ 04_tokenizer_kanish.py
@@ -3,21 +3,21 @@
   {
    "cell_type": "code",
    "execution_count": 1,
-   "id": "cd6144eb",
+   "id": "eb09732a",
    "metadata": {
     "_cell_guid": "b1076dfc-b9ad-4769-8c92-a6c4dae69d19",
     "_uuid": "8f2839f25d086af736a60e9eeb907d3b93b6e0e5",
     "execution": {
-     "iopub.execute_input": "2025-12-26T04:22:55.968748Z",
-     "iopub.status.busy": "2025-12-26T04:22:55.968158Z",
-     "iopub.status.idle": "2025-12-26T04:22:57.936954Z",
-     "shell.execute_reply": "2025-12-26T04:22:57.936087Z"
+     "iopub.execute_input": "2025-12-26T04:33:23.586747Z",
+     "iopub.status.busy": "2025-12-26T04:33:23.586019Z",
+     "iopub.status.idle": "2025-12-26T04:33:25.853066Z",
+     "shell.execute_reply": "2025-12-26T04:33:25.852184Z"
     },
     "papermill": {
-     "duration": 1.972302,
-     "end_time": "2025-12-26T04:22:57.938157",
+     "duration": 2.271212,
+     "end_time": "2025-12-26T04:33:25.854465",
      "exception": false,
-     "start_time": "2025-12-26T04:22:55.965855",
+     "start_time": "2025-12-26T04:33:23.583253",
      "status": "completed"
     },
     "tags": []
@@ -27,6 +27,33 @@
      "name": "stdout",
      "output_type": "stream",
      "text": [
+      "/kaggle/input/kanish/01_extraer.py\n",
+      "/kaggle/input/kanish/02_auditoria.py\n",
+      "/kaggle/input/kanish/publications.csv\n",
+      "/kaggle/input/kanish/04_tokenizer_kanish.py\n",
+      "/kaggle/input/kanish/07_cerebro_analitico.py\n",
+      "/kaggle/input/kanish/.gitignore\n",
+      "/kaggle/input/kanish/03_limpieza_kaggle.py\n",
+      "/kaggle/input/kanish/08_entrenar_gliner.py\n",
+      "/kaggle/input/kanish/00_populate_graph.py\n",
+      "/kaggle/input/kanish/05_pdf_miner.py\n",
+      "/kaggle/input/kanish/06_grafo_constructor.py\n",
+      "/kaggle/input/kanish/google.colab-0.1.6.vsix\n",
+      "/kaggle/input/kanish/01_kanish_export.py\n",
+      "/kaggle/input/kanish/09_finetune_nllb.py\n",
+      "/kaggle/input/kanish/train.csv\n",
+      "/kaggle/input/kanish/reporte_anomalias_kaggle.txt\n",
+      "/kaggle/input/kanish/kanish_brain_frozen.json\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/sample_submission.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/bibliography.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/publications.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/Sentences_Oare_FirstWord_LinNum.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/OA_Lexicon_eBL.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/eBL_Dictionary.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/train.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/test.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/published_texts.csv\n",
+      "/kaggle/input/kanish/deep-past-initiative-machine-translation/resources.csv\n",
       "/kaggle/input/kanish-knowledge-graph/kanish_brain_frozen.json\n"
      ]
     }
@@ -60,6 +87,11 @@
      "datasetId": 9119281,
      "sourceId": 14287115,
      "sourceType": "datasetVersion"
+    },
+    {
+     "datasetId": 9126059,
+     "sourceId": 14296693,
+     "sourceType": "datasetVersion"
     }
    ],
    "isGpuEnabled": true,
@@ -86,14 +118,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 7.574846,
-   "end_time": "2025-12-26T04:22:58.256897",
+   "duration": 8.538373,
+   "end_time": "2025-12-26T04:33:26.272787",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2025-12-26T04:22:50.682051",
+   "start_time": "2025-12-26T04:33:17.734414",
    "version": "2.6.0"
   }
  },
